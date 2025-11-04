044de2679aba5ef60db55f3f34978def22874b11
Administrator <arisue2002@gmail.com>
2025-11-04 12:45:27 +0900

    Feat : 1로그인 링크 수정 2.뱃지 데이터 수정

--- src/components/screens/BadgeScreen.jsx
+++ src/components/screens/BadgeScreen.jsx
@@ -60,7 +60,7 @@ export default function BadgeScreen({onNavigate}) {
           try {
             const res = await api.get('/badge'); // axios.js 확인
             if (res.data.status === 'SUCCESS') {
-              setbadgesList([res.data.data]);
+              setbadgesList(res.data.data); // 안돼면 [] 없애기
             } else {
               setError('뱃지 데이터를 불러오지 못했습니다.');
             }
