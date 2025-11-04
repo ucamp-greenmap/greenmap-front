5eb7594be0df862eab3a65c15b6b6f180b77a49d
yeni0727 <fullsun9600@naver.com>
2025-11-04 13:21:22 +0900

    Feat: 서비스 워커에서 GET 요청에 대한 캐시 저장 로직 추가 및 Vite 설정에 뉴스 경로에 대한 런타임 캐싱 추가

--- public/service-worker.js
+++ public/service-worker.js
@@ -26,9 +26,14 @@ self.addEventListener('fetch', (event) => {
         fetch(event.request)
             .then((response) => {
                 const responseClone = response.clone();
-                caches
-                    .open(CACHE_NAME)
-                    .then((cache) => cache.put(event.request, responseClone));
+                if (event.request.method === 'GET') {
+                    caches
+                        .open(CACHE_NAME)
+                        .then((cache) =>
+                            cache.put(event.request, responseClone)
+                        );
+                }
+
                 return response;
             })
             .catch(() => caches.match(event.request))
